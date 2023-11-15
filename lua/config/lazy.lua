b9adcfd744c9920c6fe069983b81aaeaa37ede8a
liupan01 <liupan01@bilibili.com>
2023-11-15 13:08:04 +0800

    update config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -50,13 +50,15 @@ require('gitsigns').setup({
   current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
     virt_text = true,
-    virt_text_pos = 'eol',   -- 'eol' | 'overlay' | 'right_align'
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
     delay = 100,
     ignore_whitespace = false,
     virt_text_priority = 100,
   },
 })
 
-require'lspconfig'.gopls.setup{
-  cmd = {"gopls", "-remote=unix;/tmp/gopls-daemon-socket"}
+require 'lspconfig'.gopls.setup {
+  cmd = { "gopls", "-remote=unix;/tmp/gopls-daemon-socket" }
 }
+
+require 'lspconfig'.bufls.setup {}
