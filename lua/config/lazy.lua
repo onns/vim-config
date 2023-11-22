266000204c25d66143fcdcd2cdd81b7a105b4764
liupan01 <liupan01@bilibili.com>
2023-11-22 20:53:07 +0800

    update lazyvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,7 +10,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "git@github.com:LazyVim/LazyVim.git", import = "lazyvim.plugins" },
+    { "git@github.com:onns/LazyVim.git", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
