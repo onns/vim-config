c4a8779121e596bdaecabef6bbfefb06bf11ebd6
liupan01 <liupan01@bilibili.com>
2024-03-19 11:14:32 +0800

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -254,3 +254,36 @@ vim.api.nvim_set_keymap('n', 'gcp', ':lua GetGoImportPath()<CR>', { noremap = tr
 vim.api.nvim_set_keymap('n', '<leader>fm',
     ":lua require('bookmarks').toggle_bookmarks()<CR>",
     { noremap = true, silent = true })
+
+function LiveGrepUp(n)
+    -- 获取当前工作目录
+    local cwd = vim.fn.getcwd()
+    -- 用系统的路径分隔符分割路径
+    local path_sep = package.config:sub(1, 1)
+    local components = {}
+
+    -- 分割路径到各个组件
+    for component in string.gmatch(cwd, "[^" .. path_sep .. "]+") do
+        table.insert(components, component)
+    end
+
+    -- 计算向上n级的路径
+    local up_n = #components - n
+    if up_n < 1 then up_n = 1 end -- 确保不会超出根目录
+    local new_cwd = path_sep
+    for i = 1, up_n do
+        new_cwd = new_cwd .. components[i] .. (i < up_n and path_sep or "")
+    end
+
+    -- 使用新的目录执行Telescope live_grep
+    require('telescope.builtin').live_grep({ cwd = new_cwd })
+end
+
+vim.api.nvim_create_user_command(
+    'Lg',
+    function(opts)
+        local n = tonumber(opts.args) or 0
+        LiveGrepUp(n)
+    end,
+    { nargs = 1 } -- 命令接受一个参数
+)
